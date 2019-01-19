a193b0bb8f38960c2e562808b6fdedd228d1f7e3
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-01-19 11:44:53 +0100

    Changes init

--- stores/items.js
+++ stores/items.js
@@ -1,20 +1,10 @@
 module.exports = store
 
 function store (state, emitter) {
-  state.items = []
+  state.items = require('../data.json')
   state.lastUpdated = new Date().getTime()
 
   emitter.on('DOMContentLoaded', function () {
-    emitter.on('store:init', function () {
-      if (state.items.length === 0) {
-        emitter.emit('data:load')
-      }
-    })
-    emitter.on('data:load', function () {
-      const data = require('../data.json')
-      state.items = data
-      emitter.emit(state.events.RENDER)
-    })
     emitter.on('item:postpone', function (index) {
       const item = state.items[index]
       item.lastPostponed = new Date().getTime()
@@ -23,7 +13,6 @@ function store (state, emitter) {
     emitter.on('update:all', function (index) {
       emitter.emit(state.events.RENDER)
     })
-    emitter.emit('store:init')
     setInterval(function () {
       const now = new Date().getTime()
       if (now - state.lastUpdated > 10000) {
