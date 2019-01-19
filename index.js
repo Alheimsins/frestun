c6fdf2af21a37d2604dc402b288a2dc84028140c
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-01-19 11:32:32 +0100

    Adds persist

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const css = require('sheetify')
 const choo = require('choo')
+const persist = require('choo-persist')
 
 css('tailwindcss')
 
-var app = choo()
+const app = choo()
+
 if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')())
 } else {
   app.use(require('choo-service-worker')())
+  app.use(persist())
 }
 
 app.use(require('./stores/items'))
